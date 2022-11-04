63814a0927d6a358b4d8eeb5bdf9389bbd24414d
Jacob O'Bryant <git@jacobobryant.com>
2022-11-04 13:09:07 -0700

    Remove code from worker.clj, app.clj, and schema.clj

--- src/com/eelchat/schema.clj
+++ src/com/eelchat/schema.clj
@@ -5,24 +5,10 @@
 (def schema
   {:user/id :uuid
    :user/email :string
-   :user/foo :string
-   :user/bar :string
    :user/joined-at inst?
    :user [:map {:closed true}
           [:xt/id :user/id]
           :user/email
-          :user/joined-at
-          [:user/foo {:optional true}]
-          [:user/bar {:optional true}]]
-
-   :msg/id :uuid
-   :msg/user :user/id
-   :msg/text :string
-   :msg/sent-at inst?
-   :msg [:map {:closed true}
-         [:xt/id :msg/id]
-         :msg/user
-         :msg/text
-         :msg/sent-at]})
+          :user/joined-at]})
 
 (def malli-opts {:registry (malr/composite-registry malc/default-registry schema)})
