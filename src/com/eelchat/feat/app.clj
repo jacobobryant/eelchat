bfff8fea51520b0b1ee7818865262725943c8d27
Jacob O'Bryant <git@jacobobryant.com>
2022-11-11 17:14:47 -0800

    Communities

--- src/com/eelchat/feat/app.clj
+++ src/com/eelchat/feat/app.clj
@@ -4,22 +4,67 @@
             [com.eelchat.ui :as ui]
             [xtdb.api :as xt]))
 
-(defn app [{:keys [session biff/db] :as req}]
-  (let [{:user/keys [email]} (xt/entity db (:uid session))]
-    (ui/page
-     {}
-     nil
-     [:div "Signed in as " email ". "
-      (biff/form
-       {:action "/auth/signout"
-        :class "inline"}
-       [:button.text-blue-500.hover:text-blue-800 {:type "submit"}
-        "Sign out"])
-      "."]
-     [:.h-6]
-     [:div "Thanks for joining the waitlist. "
-      "We'll let you know when eelchat is ready to use."])))
+(defn app [req]
+  (ui/app-page
+   req
+   [:p "Select a community, or create a new one."]))
+
+(defn new-community [{:keys [session] :as req}]
+  (let [comm-id (random-uuid)]
+    (biff/submit-tx req
+      [{:db/doc-type :community
+        :xt/id comm-id
+        :comm/title (str "Community #" (rand-int 1000))}
+       {:db/doc-type :membership
+        :mem/user (:uid session)
+        :mem/comm comm-id
+        :mem/roles #{:admin}}])
+    {:status 303
+     :headers {"Location" (str "/community/" comm-id)}}))
+
+(defn join-community [{:keys [user community] :as req}]
+  (biff/submit-tx req
+    [{:db/doc-type :membership
+      :db.op/upsert {:mem/user (:xt/id user)
+                     :mem/comm (:xt/id community)}
+      :mem/roles [:db/default #{}]}])
+  {:status 303
+   :headers {"Location" (str "/community/" (:xt/id community))}})
+
+(defn community [{:keys [biff/db user community] :as req}]
+  (let [member (some (fn [mem]
+                       (= (:xt/id community) (get-in mem [:mem/comm :xt/id])))
+                     (:user/mems user))]
+    (ui/app-page
+     req
+     (if member
+       [:<>
+        [:.border.border-neutral-600.p-3.bg-white.grow
+         "Messages window"]
+        [:.h-3]
+        [:.border.border-neutral-600.p-3.h-28.bg-white
+         "Compose window"]]
+       [:<>
+        [:.grow]
+        [:h1.text-3xl.text-center (:comm/title community)]
+        [:.h-6]
+        (biff/form
+         {:action (str "/community/" (:xt/id community) "/join")
+          :class "flex justify-center"}
+         [:button.btn {:type "submit"} "Join this community"])
+        [:div {:class "grow-[1.75]"}]]))))
+
+(defn wrap-community [handler]
+  (fn [{:keys [biff/db path-params] :as req}]
+    (if-some [community (xt/entity db (parse-uuid (:id path-params)))]
+      (handler (assoc req :community community))
+      {:status 303
+       :headers {"location" "/app"}})))
 
 (def features
-  {:routes ["/app" {:middleware [mid/wrap-signed-in]}
-            ["" {:get app}]]})
+  {:routes ["" {:middleware [mid/wrap-signed-in]}
+            ["/app"           {:get app}]
+            ["/community"     {:post new-community}]
+            ["/community/:id" {:middleware [wrap-community]}
+             [""      {:get community}]
+             ["/join" {:post join-community}]]]})
