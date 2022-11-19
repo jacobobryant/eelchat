e3a17d6552eb8fc6dfba296edc70ed992a7d4558
Jacob O'Bryant <git@jacobobryant.com>
2022-11-18 17:04:09 -0800

    Messages

--- src/com/eelchat/repl.clj
+++ src/com/eelchat/repl.clj
@@ -4,7 +4,21 @@
 (defn get-sys []
   (biff/assoc-db @biff/system))
 
+(defn seed-channels []
+  (let [{:keys [biff/db] :as sys} (get-sys)]
+    (biff/submit-tx sys
+      (for [[mem chan] (q db
+                          '{:find [mem chan]
+                            :where [[mem :mem/comm comm]
+                                    [chan :chan/comm comm]]})]
+        {:db/doc-type :message
+         :msg/mem mem
+         :msg/channel chan
+         :msg/created-at :db/now
+         :msg/text (str "Seed message " (rand-int 1000))}))))
+
 (comment
+  (seed-channels)
 
   ;; As of writing this, calling (biff/refresh) with Conjure causes stdout to
   ;; start going to Vim. fix-print makes sure stdout keeps going to the
@@ -13,8 +27,8 @@
 
   (let [{:keys [biff/db] :as sys} (get-sys)]
     (q db
-       '{:find (pull user [*])
-         :where [[user :user/email]]}))
+       '{:find (pull msg [*])
+         :where [[msg :msg/text]]}))
 
   (sort (keys @biff/system))
 
