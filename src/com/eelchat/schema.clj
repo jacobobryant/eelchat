1623da37209b4f043f4cf18859f47450a5e93a8e
Jacob O'Bryant <git@jacobobryant.com>
2023-04-17 16:36:08 -0700

    Inbound RSS

--- src/com/eelchat/schema.clj
+++ src/com/eelchat/schema.clj
@@ -25,10 +25,20 @@
              [:chan/title :string]
              [:chan/comm  :comm/id]]
 
+   :sub/id       :uuid
+   :subscription [:map {:closed true}
+                  [:xt/id             :sub/id]
+                  [:sub/url           :string]
+                  [:sub/chan          :chan/id]
+                  [:sub/last-post-uri {:optional true} :string]
+                  [:sub/fetched-at    {:optional true} inst?]
+                  [:sub/last-modified {:optional true} :string]
+                  [:sub/etag          {:optional true} :string]]
+
    :msg/id  :uuid
    :message [:map {:closed true}
              [:xt/id          :msg/id]
-             [:msg/mem        :mem/id]
+             [:msg/mem        [:or :mem/id [:enum :system]]]
              [:msg/text       :string]
              [:msg/channel    :chan/id]
              [:msg/created-at inst?]]})
