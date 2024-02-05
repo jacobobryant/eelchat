3437137c10c6fa536589be128d4b758b122ba404
Jacob O'Bryant <hello@tfos.co>
2024-02-04 19:02:22 -0800

    Create new Biff project (v1.0.0)

---
+++ README.md
@@ -0,0 +1,7 @@
+# Biff example project
+
+This is the example project for Biff.
+
+Run `clj -M:dev dev` to get started. See `clj -M:dev --help` for other commands.
+
+Consider adding `alias biff='clj -M:dev'` to your `.bashrc`.
---
+++ test/com/eelchat_test.clj
@@ -0,0 +1,6 @@
+(ns com.eelchat-test
+  ;; If you add more test files, require them here so that they'll get loaded by com.eelchat/on-save
+  (:require [clojure.test :refer [deftest is]]))
+
+(deftest example-test
+  (is (= 4 (+ 2 2))))
