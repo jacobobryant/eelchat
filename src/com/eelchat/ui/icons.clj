7016cd4c6fc5c081aacdfbb86627866b9d953225
Jacob O'Bryant <git@jacobobryant.com>
2024-05-25 15:25:42 -0700

    Channels

---
+++ src/com/eelchat/ui/icons.clj
@@ -0,0 +1,13 @@
+(ns com.eelchat.ui.icons)
+
+(def data
+  {:x {:view-box "0 0 384 512", :path "M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"}})
+
+(defn icon [k & [opts]]
+  (let [{:keys [view-box path]} (data k)]
+    [:svg.flex-shrink-0.inline
+     (merge {:xmlns "http://www.w3.org/2000/svg"
+             :viewBox view-box}
+            opts)
+     [:path {:fill "currentColor"
+             :d path}]]))
